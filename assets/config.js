942e53dc40bbd6bd2a130d39407e2d51baeb28da
Dean Schulz <dean.schulz@americaneagle.com>
2024-05-27 22:20:54 -0500

    files added

---
+++ assets/config.js
@@ -0,0 +1,40 @@
+// cpanel - site_templates/vcard_dev/assets/config.js.tt Copyright(c) 2016 cPanel, Inc.
+//                                                          All rights Reserved.
+// copyright@cpanel.net                                        http://cpanel.net
+// This code is subject to the cPanel license. Unauthorized copying is prohibited
+
+window.cpanel = {
+    data: {
+        phone: "",
+        fax: "",
+        email: "dean@deanpschulz.com",
+        address: "",
+        social: [
+            
+            
+            
+            
+            
+            
+        ]
+    },
+    style: {
+        primary: "",
+        accent: "#1bd800"
+    },
+    slides: [
+        {
+            prefix: "My name is",
+            title: "Dean Schulz",
+            subtitle: "Web Developer",
+            type: 'vcard',
+            backgroundImage: "",
+            backgroundColor: "",
+            color: "#8eff91",
+            buttonText: "",
+            buttonLink: "",
+            biography: "",
+            portraitImage: "\/assets\/images\/dev\/profile.jpg"
+        }
+    ]
+};
